4d9194ed78c8fa0049cf513052a550251503ee14
gyu-don <takumi.kt+git@gmail.com>
2017-03-31 22:16:15 +0900

    dev start. (still not ok)

---
+++ word_analogy/src/main.rs
@@ -0,0 +1,78 @@
+extern crate clap;
+
+use std::{cmp, f64, fs, io};
+use std::collections::HashMap;
+use std::io::{BufRead, Error, ErrorKind};
+use clap::{App, Arg};
+
+macro_rules! some {
+    ( $e : expr ) => { match $e { Some(x) => x, None => return None } }
+}
+
+fn generate(vocab_file: &str, vectors_file: &str) -> Result<HashMap<String, Vec<f64>>, Error> {
+    let mut map = HashMap::<String, Vec<f64>>::new();
+    let mut f = io::BufReader::new(fs::File::open(vectors_file)?);
+    let mut line = String::new();
+    while f.read_line(&mut line).is_ok() {
+        let s = line.trim();
+        if s.len() == 0 { break; }
+        let mut it = s.split_whitespace();
+        let w = it.next().ok_or(Error::new(ErrorKind::InvalidData, "Bad line"))?;
+        let mut v = Vec::<f64>::new();
+        for x in it {
+            match x.parse::<f64>() {
+                Ok(val) => v.push(val),
+                Err(e) => return Err(Error::new(ErrorKind::InvalidData, e)),
+            }
+        }
+        map.insert(w.to_string(), v);
+    }
+    // normalize: pending
+    Ok(map)
+}
+
+fn word_analogy<'a>(map: &'a HashMap<String, Vec<f64>>, w1: &str, w2: &str, w3: &str) -> Option<Vec<(&'a str, f64)>> {
+    let mut vec = some!(map.get(w1)).clone();
+    let it2 = some!(map.get(w2)).iter();
+    for (x, y) in vec.iter_mut().zip(it2) { *x += *y; }
+    let it3 = some!(map.get(w3)).iter();
+    for (x, y) in vec.iter_mut().zip(it3) { *x -= *y; }
+    let mut wv: Vec<(&'a str, f64)> = map.iter().map(|(w, v)| {
+        if w != w1 && w != w2 && w != w3 {
+            // inner product
+            (w.as_str(), vec.iter().zip(v).map(|(x, y)| x * y).sum())
+        }
+        else { (w.as_str(), f64::NEG_INFINITY) }
+    }).collect();
+    wv.sort_by(|x,y| (x.1).partial_cmp(&y.1).unwrap_or(cmp::Ordering::Less));
+    Some(wv)
+}
+
+fn main() {
+    let matches = App::new("distance")
+        .about("Get distance between 2 vectors.")
+        .arg(Arg::with_name("vocab_file")
+            .default_value("vocab.txt"))
+        .arg(Arg::with_name("vectors_file")
+             .default_value("vectors.txt"))
+        .get_matches();
+
+    println!("vocab_file: {}", matches.value_of("vocab_file").unwrap());
+    println!("vectors_file: {}", matches.value_of("vectors_file").unwrap());
+    let word_vector = generate(matches.value_of("vocab_file").unwrap(),
+                            matches.value_of("vectors_file").unwrap()).unwrap();
+    let mut line = String::new();
+    println!("Enter 3 words (EXIT to break)");
+    while io::stdin().read_line(&mut line).is_ok() {
+        let line = line.trim().to_string();
+        if line == "EXIT" { break; }
+        let words: Vec<_> = line.split_whitespace().collect();
+        if words.len() == 3 {
+            let v = word_analogy(&word_vector, words[0], words[1], words[2]);
+            for (x, _) in v.unwrap().iter().zip(0 .. 100) {
+                println!("{:?}", x);
+            }
+        }
+        println!("Enter 3 words (EXIT to break)");
+    }
+}
